c178f6a9421198926e1db3d3bef79d8e818b89a9
Adam Janiš <adam.janis@gmail.com>
2022-09-27 22:43:34 +0100

    fixes

--- cron/repeat.ts
+++ cron/repeat.ts
@@ -1,13 +1,15 @@
 export default {
 	async cron(cron: Repeat.Cron, env: Repeat.Env, ctx: ExecutionContext): Promise<void> {
 		try {
+			console.log('running cron job');
 			// write cron task here
 
 			// track success
+			// metrics can be viewed in "Metrics tab"
 			env.metrics.write('cron_processed', 1, 'success');
 		} catch (e) {
 			// log error
-			console.error('Cron failed!', e.message);
+			console.error('cron failed!', e.message);
 
 			// track failure
 			env.metrics.write('cron_processed', 1, 'failure');
--- webhook/repeat.ts
+++ webhook/repeat.ts
@@ -1,8 +1,16 @@
 export default {
 	async webhook(request: Request, env: Repeat.Env, ctx: ExecutionContext) {
-		console.log('incoming request', request.method, request.url);
+		console.log('incoming request', `${request.method} ${request.url}`);
+
+		// write request to metrics, label with request method
+		env.metrics.write('request', 1, request.method);
 
 		if (request.headers.get('x-webhook-secret') !== env.variables.WEBHOOK_SECRET) {
+			// write failed auth
+			env.metrics.write('request_auth_failed', 1);
+
+			// log to console
+			console.warn('invalid webhook secret');
 			return Response.json({ success: false, error: 'invalid webhook secret' }, { status: 401 });
 		}
 
