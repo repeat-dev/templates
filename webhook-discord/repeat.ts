85bc902281c2727c6e16eb9fcc290a7a1a679cfb
Adam Janiš <adam.janis@gmail.com>
2022-11-06 14:28:00 +0000

    add text/plain support

--- webhook-discord/repeat.ts
+++ webhook-discord/repeat.ts
@@ -1,7 +1,7 @@
 // Set WEBHOOK_SECRET in Variables 👉
 
 // list of headers to hide value for
-const redactedHeaders = ['authorization'];
+const redactedHeaders = ['authorization', 'cf-connecting-ip', 'x-real-ip'];
 
 export default {
 	async webhook(request: Request, env: Repeat.Env) {
@@ -77,20 +77,28 @@ async function requestToDiscord(request: Request, env: Repeat.Env) {
 	};
 
 	formData.append('payload_json', JSON.stringify(message));
-	
+
 	let blob;
+	let blobFileExtension;
 	if (request.headers.get('content-type')?.includes('application/json')) {
 		blob = new Blob([JSON.stringify(await request.json(), null, 2)], {
 			type: 'application/json',
 		});
+		blobFileExtension = 'json';
 	} else if (request.headers.get('content-type')?.includes('multipart/form-data')) {
 		blob = new Blob([JSON.stringify(Object.fromEntries(await request.formData()), null, 2)], {
 			type: 'application/json',
 		});
+		blobFileExtension = 'json';
+	} else if (request.headers.get('content-type')?.includes('text/')) {
+		blob = new Blob([await request.text()], {
+			type: 'text/plain',
+		});
+		blobFileExtension = 'txt';
 	}
 
 	if (blob) {
-		formData.append('file', blob, 'body.json');
+		formData.append('file', blob, `body.${blobFileExtension}`);
 	}
 
 	// send webhook
