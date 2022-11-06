6f732360353cd26d2d138470ffe58eacc201fc12
Adam Janiš <adam.janis@gmail.com>
2022-11-06 13:39:33 +0000

    new: webhook to discord template

---
+++ webhook-discord/repeat.ts
@@ -0,0 +1,101 @@
+// Set WEBHOOK_SECRET in Variables 👉
+
+// list of headers to hide value for
+const redactedHeaders = ['authorization'];
+
+export default {
+	async webhook(request: Request, env: Repeat.Env) {
+		// clone, parse and send request to Discord
+		const requestCloned = request.clone();
+		await requestToDiscord(requestCloned, env);
+
+		// do something with original request as you please
+		// await fetch("https://my-service.example.com", request)
+	},
+};
+
+async function requestToDiscord(request: Request, env: Repeat.Env) {
+	const url = new URL(request.url);
+	let searchParamsFormatted = '';
+	let headersFormatted = '';
+
+	// format search params
+	url.searchParams.forEach((value, key) => {
+		searchParamsFormatted += `${key} = ${value}\n`;
+	});
+
+	// format headers
+	request.headers.forEach((value, key) => {
+		headersFormatted += `${key} = ${redactedHeaders.includes(key) ? '*REDACTED*' : value}\n`;
+	});
+
+	// construct embeds
+	const embeds = [
+		{
+			title: 'Webhook details',
+			color: 5814783,
+			fields: [
+				{
+					name: 'Method',
+					value: request.method,
+					inline: true,
+				},
+				{
+					name: 'Host',
+					value: url.hostname,
+					inline: true,
+				},
+				{
+					name: 'Path',
+					value: url.pathname,
+					inline: true,
+				},
+				searchParamsFormatted
+					? {
+							name: 'Search params',
+							value: searchParamsFormatted,
+					  }
+					: undefined,
+				headersFormatted
+					? {
+							name: 'Headers',
+							value: headersFormatted,
+					  }
+					: undefined,
+			].filter(Boolean),
+			timestamp: new Date().toISOString(),
+		},
+	];
+
+	// construct discord message FormData
+	const formData = new FormData();
+
+	const message = {
+		username: 'repeat.dev',
+		content: null,
+		embeds,
+	};
+
+	formData.append('payload_json', JSON.stringify(message));
+	
+	let blob;
+	if (request.headers.get('content-type')?.includes('application/json')) {
+		blob = new Blob([JSON.stringify(await request.json(), null, 2)], {
+			type: 'application/json',
+		});
+	} else if (request.headers.get('content-type')?.includes('multipart/form-data')) {
+		blob = new Blob([JSON.stringify(Object.fromEntries(await request.formData()), null, 2)], {
+			type: 'application/json',
+		});
+	}
+
+	if (blob) {
+		formData.append('file', blob, 'body.json');
+	}
+
+	// send webhook
+	await fetch(env.variables.WEBHOOK_SECRET, {
+		method: 'POST',
+		body: formData,
+	});
+}
